f67f8aaea0b1973c1b46af666f38fc918983b4f7
Thiago Ferreira <thiagofb84jp@gmail.com>
2024-07-30 17:29:06 -0300

    Including new classes

--- 8 - Classes/index.js
+++ 8 - Classes/index.js
@@ -6,3 +6,106 @@ const user = new User();
 console.log(user);
 user.name = "Francisco";
 console.log(user);
+// 2 - Constructor
+class Employeer {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+const john = new Employeer("John Appleseed", 25);
+console.log(john);
+// 3 - Campo readonly
+class Car {
+    constructor(name) {
+        this.wheels = 4;
+        this.name = name;
+    }
+}
+const ferrari = new Car("Ferrari");
+console.log(ferrari);
+console.log(ferrari.wheels);
+console.log((ferrari.name = "Ferrari Turbo"));
+// 4 - Herança e Super
+class Machine {
+    constructor(name) {
+        this.name = name;
+    }
+}
+const trator = new Machine("Trator");
+class KillerMachine extends Machine {
+    constructor(name, guns) {
+        super(name);
+        this.guns = guns;
+    }
+}
+const destroyer = new KillerMachine("Destroyer", 4);
+console.log(trator);
+console.log(destroyer);
+// 5 - Métodos
+class Dwarf {
+    constructor(name) {
+        this.name = name;
+    }
+    greeting() {
+        console.log("Hey Stranger!");
+    }
+}
+const jimmy = new Dwarf("Jimmy");
+console.log(jimmy.name);
+jimmy.greeting();
+console.log(jimmy);
+// 6 - This
+class Truck {
+    constructor(model, hp) {
+        this.model = model;
+        this.hp = hp;
+    }
+    showDetails() {
+        console.log(`Caminhão do modelo: ${this.model}, que tem ${this.hp} cavalos de potência!`);
+    }
+}
+const volvo = new Truck("Volvo", 400);
+const scania = new Truck("Scania", 500);
+volvo.showDetails();
+scania.showDetails();
+// 7 - Getters
+class Person {
+    constructor(name, surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+    get fullName() {
+        return this.name + " " + this.surname;
+    }
+}
+const maryOlson = new Person("Mary", "Olson");
+console.log(maryOlson.name);
+console.log(maryOlson.surname);
+console.log(maryOlson.fullName);
+// 8 - Setters
+class Coords {
+    set fillX(x) {
+        if (x === 0) {
+            return;
+        }
+        this.x = x;
+        console.log("X inserido com sucesso.");
+    }
+    set fillY(y) {
+        if (y === 0) {
+            return;
+        }
+        this.y = y;
+        console.log("Y inserido com sucesso.");
+    }
+    get getCoords() {
+        return `X: ${this.x} e Y: ${this.y}`;
+    }
+}
+const myCoords = new Coords();
+myCoords.fillX = 15;
+myCoords.fillY = 0;
+myCoords.fillY = 10;
+console.log(myCoords);
+console.log(myCoords.getCoords);
--- 8 - Classes/index.ts
+++ 8 - Classes/index.ts
@@ -12,3 +12,163 @@ console.log(user);
 user.name = "Francisco";
 
 console.log(user);
+
+// 2 - Constructor
+class Employeer {
+  name;
+  age;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const john = new Employeer("John Appleseed", 25);
+console.log(john);
+
+// 3 - Campo readonly
+class Car {
+  name;
+  readonly wheels = 4;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+const ferrari = new Car("Ferrari");
+
+console.log(ferrari);
+console.log(ferrari.wheels);
+
+console.log((ferrari.name = "Ferrari Turbo"));
+
+// 4 - Herança e Super
+class Machine {
+  name;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+const trator = new Machine("Trator");
+
+class KillerMachine extends Machine {
+  guns;
+
+  constructor(name: string, guns: number) {
+    super(name);
+    this.guns = guns;
+  }
+}
+
+const destroyer = new KillerMachine("Destroyer", 4);
+
+console.log(trator);
+console.log(destroyer);
+
+// 5 - Métodos
+class Dwarf {
+  name;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  greeting() {
+    console.log("Hey Stranger!");
+  }
+}
+
+const jimmy = new Dwarf("Jimmy");
+
+console.log(jimmy.name);
+
+jimmy.greeting();
+
+console.log(jimmy);
+
+// 6 - This
+class Truck {
+  model;
+  hp;
+
+  constructor(model: string, hp: number) {
+    this.model = model;
+    this.hp = hp;
+  }
+
+  showDetails() {
+    console.log(
+      `Caminhão do modelo: ${this.model}, que tem ${this.hp} cavalos de potência!`
+    );
+  }
+}
+
+const volvo = new Truck("Volvo", 400);
+const scania = new Truck("Scania", 500);
+
+volvo.showDetails();
+scania.showDetails();
+
+// 7 - Getters
+class Person {
+  name;
+  surname;
+
+  constructor(name: string, surname: string) {
+    this.name = name;
+    this.surname = surname;
+  }
+
+  get fullName() {
+    return this.name + " " + this.surname;
+  }
+}
+
+const maryOlson = new Person("Mary", "Olson");
+console.log(maryOlson.name);
+console.log(maryOlson.surname);
+console.log(maryOlson.fullName);
+
+// 8 - Setters
+class Coords {
+  x!: number;
+  y!: number;
+
+  set fillX(x: number) {
+    if (x === 0) {
+      return;
+    }
+
+    this.x = x;
+
+    console.log("X inserido com sucesso.");
+  }
+
+  set fillY(y: number) {
+    if (y === 0) {
+      return;
+    }
+
+    this.y = y;
+
+    console.log("Y inserido com sucesso.");
+  }
+
+  get getCoords() {
+    return `X: ${this.x} e Y: ${this.y}`;
+  }
+
+}
+
+const myCoords = new Coords();
+
+myCoords.fillX = 15;
+myCoords.fillY = 0;
+myCoords.fillY = 10;
+
+console.log(myCoords);
+console.log(myCoords.getCoords);
