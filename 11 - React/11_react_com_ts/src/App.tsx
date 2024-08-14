7ef7e2539102a4b9275a9d60dc77c5ea01a2a326
Thiago Ferreira <thiagofb84jp@gmail.com>
2024-08-14 17:53:06 -0300

    Including new files

--- 11 - React/11_react_com_ts/src/App.tsx
+++ 11 - React/11_react_com_ts/src/App.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+// 4 - Componentes
+import FirstComponent from "./components/FirstComponent";
 
 function App() {
+  // 1 - Variáveis
+  const name: string = "James";
+  const age: number = 39;
+  const isWorking: boolean = true;
+
+  // 2 - Funções
+  const userGreeting = (name: string): string => {
+    return `Olá, ${name}! Como estão as coisas?`;
+  };
+
   return (
     <div className="App">
       <h1>Hello World!!</h1>
       <h1>Typescript com React!</h1>
+      <h2>::General Data::</h2>
+      <h3>Name: {name}</h3>
+      <h3>Number: {age}</h3>
+      {isWorking && (
+        <div>
+          <p>Está trabalhando!</p>
+        </div>
+      )}
+      <h3>{userGreeting(name)}</h3>
+      <FirstComponent />
+      <FirstComponent />
     </div>
   );
 }
