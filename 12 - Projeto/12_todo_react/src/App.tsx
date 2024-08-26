c4b21a3b72029c93975ad66a25a4fc90d4347135
Thiago Ferreira <thiagofb84jp@gmail.com>
2024-08-26 16:33:03 -0300

    Including new resources

--- 12 - Projeto/12_todo_react/src/App.tsx
+++ 12 - Projeto/12_todo_react/src/App.tsx
@@ -15,6 +15,14 @@ import { ITask } from "./interface/Task";
 function App() {
   const [taskList, setTaskList] = useState<ITask[]>([]);
 
+  const deleteTask = (id: number) => {
+    setTaskList(
+      taskList.filter((task) => {
+        return task.id !== id;
+      })
+    );
+  };
+
   return (
     <div>
       <Header />
@@ -29,7 +37,7 @@ function App() {
         </div>
         <div>
           <h2>Suas Tarefas:</h2>
-          <TaskList />
+          <TaskList taskList={taskList} handleDelete={deleteTask} />
         </div>
       </main>
       <Footer />
--- 12 - Projeto/12_todo_react/src/components/TaskList.tsx
+++ 12 - Projeto/12_todo_react/src/components/TaskList.tsx
@@ -1,12 +1,39 @@
 import React from "react";
 
-interface Props {}
+// Interfaces
+import { ITask } from "../interface/Task";
 
-const TaskList = (props: Props) => {
+// CSS
+import styles from "./TaskList.module.css";
+
+interface Props {
+  taskList: ITask[];
+  handleDelete(id: number): void;
+}
+
+const TaskList = ({ taskList, handleDelete }: Props) => {
   return (
-    <div>
-      <p>Lista de Tarefas</p>
-    </div>
+    <>
+      {taskList.length > 0 ? (
+        taskList.map((task) => (
+          <div key={task.id} className={styles.task}>
+            <div className={styles.details}>
+              <h4>{task.title}</h4>
+              <p>Dificuldade: {task.difficulty}</p>
+            </div>
+            <div className={styles.actions}>
+              <i className="bi bi-pencil"></i>
+              <i
+                className="bi bi-trash"
+                onClick={() => handleDelete(task.id)}
+              ></i>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>Não há tarefas cadastradas.</p>
+      )}
+    </>
   );
 };
 
