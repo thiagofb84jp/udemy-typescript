5f035b6fe1852538e88a44f69a0dfddc5922315a
Thiago Ferreira <thiagofb84jp@gmail.com>
2024-09-05 06:03:06 -0300

    Including new files

--- 13 - Express/13_express_com_TS/src/app.ts
+++ 13 - Express/13_express_com_TS/src/app.ts
@@ -2,12 +2,20 @@
 console.log("Express + TS!");
 
 // 2 - Utilizanod o Express
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 
 const app = express();
 
 // 3 - Rota com POST
 app.use(express.json());
+
+// 11 - Middleware para todas as rotas
+function showPath(req: Request, res: Response, next: NextFunction) {
+  console.log(req.path);
+  next();
+}
+
+app.use(showPath);
 
 app.get("/", (req, res) => {
   return res.send("Hello Express!");
@@ -66,6 +74,53 @@ app.get("/api/product/:id", (req: Request, res: Response) => {
 
   return res.send("Produto não encontrado.");
 });
+
+// 8 - Rotas Complexas
+app.get("/api/product/:id/review/:reviewId", (req: Request, res: Response) => {
+  console.log(req.params);
+
+  const productId = req.params.id;
+  const reviewId = req.params.reviewId;
+
+  return res.send(`Acessando a review ${reviewId} do produto ${productId}.`);
+});
+
+// 9 - Router Handler
+function getUser(req: Request, res: Response) {
+  console.log(`Resgatando o usuário com id: ${req.params.id}`);
+
+  return res.send("O usuário foi encontrado.");
+}
+
+app.get("/api/user/:id", getUser);
+
+// 10 - Middleware
+function checkUser(req: Request, res: Response, next: NextFunction) {
+  if (req.params.id === "1") {
+    console.log("Pode seguir.");
+    next();
+  } else {
+    console.log("Acesso restrito. Por favor, tente outra rota.");
+  }
+}
+
+app.get("/api/user/:id/access", checkUser, (req: Request, res: Response) => {
+  return res.json({ msg: "Bem-vindo a área administrativa." });
+});
+
+// 12 - Req e Res com Generics
+app.get(
+  "/api/user/:id/details/:name",
+  (
+    req: Request<{ id: string; name: string }>,
+    res: Response<{ status: boolean }>
+  ) => {
+    console.log(`ID: ${req.params.id}`);
+    console.log(`Name: ${req.params.name}`);
+
+    return res.json({ status: true });
+  }
+);
 
 app.listen(3000, () => {
   console.log("Hello Express!");
