840894b234f7116916c003fd5adae324ec693eb1
Thiago Ferreira <thiagofb84jp@gmail.com>
2024-09-18 06:16:27 -0300

    Including new files

--- 14 - API-Restful/14_API_RESTFUL_COM_TS/config/default.ts
+++ 14 - API-Restful/14_API_RESTFUL_COM_TS/config/default.ts
@@ -3,6 +3,6 @@ const dbPassword = process.env.DB_PASS;
 
 export default {
   port: 3000,
-  dbUri: `mongodb+srv://${dbUser}:${dbPassword}@udemy.p7dyt.mongodb.net/?retryWrites=true&w=majority&appName=udemy`,
+  dbUri: `mongodb+srv://${dbUser}:${dbPassword}@udemy.p7dyt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&appName=udemy`,
   env: "development",
 };
--- 14 - API-Restful/14_API_RESTFUL_COM_TS/src/router.ts
+++ 14 - API-Restful/14_API_RESTFUL_COM_TS/src/router.ts
@@ -1,7 +1,10 @@
 import { Router, Request, Response } from "express";
+import { createMovie } from "./controllers/movieControllers";
 
 const router = Router();
 
-export default router.get("/test", (req: Request, res: Response) => {
-  res.status(200).send("API is Working!");
-});
+export default router
+  .get("/test", (req: Request, res: Response) => {
+    res.status(200).send("API is Working!");
+  })
+  .post("/movie", createMovie);
