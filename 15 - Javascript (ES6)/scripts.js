7eb37abe022f18a165a7ac06d32aeb16f5989e72
Thiago Ferreira <thiagofb84jp@gmail.com>
2024-09-27 06:34:54 -0300

    Including new topics

--- 15 - Javascript (ES6)/scripts.js
+++ 15 - Javascript (ES6)/scripts.js
@@ -175,4 +175,67 @@ const a3 = [...a1, ...a2];
 
 console.log(a3);
 
-const a4 = [0];
+const a4 = [0, ...a1, 4];
+
+console.log(4);
+
+const carName = { name: "Gol" };
+const carBrand = { brand: "VW" };
+const otherInfos = { km: 10000, price: 49000 };
+
+const car = { ...carName, ...carBrand, ...otherInfos, wheels: 4 };
+
+console.log(car);
+
+// 8 - Classe
+class Product {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+
+  productWithDiscount(discount) {
+    return this.price * ((100 - discount) / 100);
+  }
+}
+
+const shirt = new Product("Camisa Gola V", 20);
+
+console.log(shirt.name);
+
+console.log(shirt.productWithDiscount(10));
+
+console.log(shirt.productWithDiscount(50));
+
+const tenis = new Product("Tênis Verde", 120);
+
+console.log(tenis.name);
+
+console.log(tenis.productWithDiscount(20));
+
+// 9 - Herança
+class ProductWithAttributes extends Product {
+  constructor(name, price, colors) {
+    super(name, price);
+    this.colors = colors;
+  }
+
+  showColors() {
+    console.log("As cores são: ");
+    this.colors.forEach((color) => {
+      console.log(color);
+    });
+  }
+}
+
+const hat = new ProductWithAttributes("Chapéu", 29.99, [
+  "Preto",
+  "Azul",
+  "Verde",
+]);
+
+console.log(hat.name);
+
+console.log(hat.productWithDiscount(30));
+
+hat.showColors();
